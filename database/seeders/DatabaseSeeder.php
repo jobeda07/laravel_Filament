e890c26b346f85ab059aa5983683d0ddf0438096
jobeda <jobedakhatun710@gmail.com>
2025-01-28 17:54:21 +0600

    student module

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Student;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Liza',
+            'email' => 'liza@gmail.com',
+            'password' => bcrypt('12345678'),
         ]);
+        Student::factory(10)->create();
     }
 }
